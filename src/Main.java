93cf6b8d301654ee307d451874d21e1ed5d84b93
Ernar <230152@astanait.edu.kz>
2024-04-24 15:21:52 +0500

    MyArrayList finished (MB)

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,22 @@
 public class Main {
     public static void main(String[] args) {
 
-        MyArrayList arrayList = new MyArrayList();
+        MyArrayList arrayList = new MyArrayList(); // [5, 9, 4, 7, 3, 7]
         arrayList.add(5);
         arrayList.add(9);
-        arrayList.add(6);
+        arrayList.add(7);
         arrayList.add(3);
         arrayList.add(7);
-        arrayList.remove(2);
-        System.out.println(arrayList.getLast());
+
+        arrayList.add(2, 4);
+
+        System.out.println(arrayList.getFirst());
+        System.out.println(arrayList.get(3));
+        System.out.println(arrayList.lastIndexOf(7));
+        System.out.println(arrayList.size());
+        arrayList.printArr();
+        arrayList.sort();
+        System.out.println();
+        arrayList.printArr();
     }
 }
--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -16,15 +16,22 @@ public class MyArrayList<T> implements myList<T> {
         }
         arr = newArr;
     }
+
     private void checkIndex(int index) {
-        if (index < 0 || index >= size){
+        if (index < 0 || index >= size) {
             throw new ArrayIndexOutOfBoundsException("Index is not correct!");
+        }
+    }
+    public void printArr(){
+        for (int i = 0; i<size; i++){
+            System.out.print(arr[i]);
+            System.out.print(" ");
         }
     }
 
     @Override
     public void add(T item) {
-        if(size>=arr.length){
+        if (size >= arr.length) {
             increaseBuffer();
         }
         arr[size++] = item;
@@ -33,7 +40,7 @@ public class MyArrayList<T> implements myList<T> {
     @Override
     public void set(int index, T item) {
         checkIndex(index);
-        if(size>=arr.length){
+        if (size >= arr.length) {
             increaseBuffer();
         }
         arr[index] = item;
@@ -41,7 +48,14 @@ public class MyArrayList<T> implements myList<T> {
 
     @Override
     public void add(int index, T item) {
-
+        checkIndex(index);
+        if (size >= arr.length) {
+            increaseBuffer();
+        }
+        for (int i = size - 1; i <= index; i--) {
+            arr[i + 1] = arr[i];
+        }
+        arr[index] = item;
     }
 
     @Override
@@ -52,7 +66,7 @@ public class MyArrayList<T> implements myList<T> {
 
     @Override
     public void addLast(T item) {
-        arr[arr.length-1] = item;
+        arr[arr.length - 1] = item;
     }
 
     @Override
@@ -68,51 +82,74 @@ public class MyArrayList<T> implements myList<T> {
 
     @Override
     public T getLast() {
-        return arr[arr.length-1];
+        return arr[arr.length - 1];
     }
 
     @Override
     public void remove(int index) {
         checkIndex(index);
-        for (int i = index+1; i<size; i++){
-            arr[i-1] = arr[i];
+        for (int i = index + 1; i < size; i++) {
+            arr[i - 1] = arr[i];
         }
         size--;
     }
 
     @Override
     public void removeFirst() {
-        for (int i = 0; i<size; i++){
-            arr[i-1] = arr[i];
+        for (int i = 0; i < size; i++) {
+            arr[i - 1] = arr[i];
         }
         size--;
     }
 
     @Override
     public void removeLast() {
-        for (int i = size-1; i<size; i++){
-            arr[i-1] = arr[i];
+        for (int i = size - 1; i < size; i++) {
+            arr[i - 1] = arr[i];
         }
         size--;
     }
 
     @Override
     public void sort() {
-
+        for (int i = 0; i < size - 1; i++) {
+            for (int j = 0; j < size - i - 1; j++) {
+                if (((Comparable<T>) arr[j]).compareTo(arr[j + 1]) > 0) {
+                    T temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
     }
 
     @Override
     public int indexOf(Object object) {
-        return 0;
+        for (int i = 0; i < size; i++) {
+            if (arr[i] == object) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     @Override
     public int lastIndexOf(Object object) {
-        return 0;
+        for (int i = size - 1; i != 0; i--) {
+            if (arr[i] == object) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     @Override
     public boolean exists(Object object) {
+        for (int i = 0; i < size; i++) {
+            if (arr[i] == object) {
+                return true;
+            }
+        }
         return false;
     }
 
@@ -123,12 +160,13 @@ public class MyArrayList<T> implements myList<T> {
 
     @Override
     public void clear() {
-
+        arr = (T[]) new Object[5];
+        size = 0;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
